73d76ab85cff7093827db84b86ad6cefdc8673be
Dejan Tomic <hi@iamdejan.com>
2026-07-24 15:55:45 +0200

    back: the assets item ships the front bundle to the browser

---
+++ back/items/onetype/assets/commands.js
@@ -0,0 +1,7 @@
+onetype.assets.ItemAdd({
+    id: 'commands',
+    description: 'The commands addon front, the door to every action.',
+    addon: 'commands',
+    js: '../../../front',
+    url: import.meta.url
+});
