18db1b026b5960c54ec5cfad0ce6709fcedbd676
Dejan Tomic <hi@iamdejan.com>
2026-07-24 15:55:45 +0200

    front: exposed run runs locally, run.api runs on the server over http

---
+++ front/functions/exposed/run.api.js
@@ -0,0 +1,34 @@
+commands.FnExpose('run.api', async function(id, data = {})
+{
+    try
+    {
+        const response = await fetch('/api/commands/run', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id, data })
+        });
+
+        const result = await response.json();
+
+        if(result.code !== 200)
+        {
+            return {
+                data: null,
+                message: result.message,
+                code: result.code
+            };
+        }
+
+        return result.data;
+    }
+    catch(error)
+    {
+        return {
+            data: null,
+            message: error.message,
+            code: 500
+        };
+    }
+});
---
+++ front/functions/exposed/run.js
@@ -0,0 +1,11 @@
+commands.FnExpose('run', async function(id, data = {})
+{
+    const command = commands.ItemGet(id);
+
+    if(!command)
+    {
+        throw onetype.Error(404, 'Command :id: not found.', {id});
+    }
+
+    return await command.Fn('run', data);
+});
